5a8b3ae8a625166789c1c66d0dd0d13fe766453c
karansinghrajpurohit <karansinghrajpurohit1999@gmail.com>
2022-06-11 18:02:00 +0530

    Base Code Setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA4ZRH4ysUDKFL1iDHtFs2BmmXT2R5NL3E',
+    authDomain: 'notification-test-8be5b.firebaseapp.com',
+    projectId: 'notification-test-8be5b',
+    storageBucket: 'notification-test-8be5b.appspot.com',
+    messagingSenderId: '1006560389889',
+    appId: '1:1006560389889:web:438f634c2dd162f0430900',
+    measurementId: 'G-CWRZM2YTQX',
+  },
+  backendURL: 'https://sarveksana.ml',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA4ZRH4ysUDKFL1iDHtFs2BmmXT2R5NL3E',
+    authDomain: 'notification-test-8be5b.firebaseapp.com',
+    projectId: 'notification-test-8be5b',
+    storageBucket: 'notification-test-8be5b.appspot.com',
+    messagingSenderId: '1006560389889',
+    appId: '1:1006560389889:web:438f634c2dd162f0430900',
+    measurementId: 'G-CWRZM2YTQX',
+  },
+  backendURL: 'http://localhost:8087',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
